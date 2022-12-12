16826e24cba2fd6a6e609831ed193baa90a1a313
MorikawaRei <93118883+MorikawaRei@users.noreply.github.com>
2022-12-12 20:30:26 +0900

    initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -8,7 +10,20 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(
+			{
+				// default options are shown. On some platforms
+				// these options are set automatically — see below
+				pages: 'build',
+				assets: 'build',
+				fallback: null,
+				precompress: false
+			}
+		),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
